fd35ef5408b840829ce545e70b80871179254ff0
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-27 20:40:23 -0600

    multiple cities in the map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -10,21 +10,60 @@ let map = L.map('mapid', {
     zoom: 14
 }); 
 
-//Add a marker to the map for Los Angeles, California
-// let marker = L.circle([34.0522, -118.2437], {
-//     radius: 100
-// }).addTo(map)
+//Add an array containing each city's location, state, and population
+let cities =[{
+    location: [40.7128, -74.0059],
+    city: 'New York City',
+    state: 'NY',
+    population: 8398748
+},
+{
+location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
+
+// for (let i=0; i < cities.length; i++)
+
+cities.forEach(city => console.log(city))
+
+// add cities to the map by adding to the marker function
+cities.forEach(city => {
+    console.log(city)
+    L.marker(city.location)
+    .addTo(map);
+})
 
 // Skill Drill
-let marker = L.circle([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: 'lightyellow',
-    weight: 3,
-    radius: 300
-}).addTo(map)
+// let marker = L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: 'lightyellow',
+//     weight: 3,
+//     radius: 300
+// }).addTo(map)
 
-//Add a marker too the map for Winnipeg, MB
-let markerWin = L.marker([49.8951, -97.1384]).addTo(map)
+// //Add a marker too the map for Winnipeg, MB
+// let markerWin = L.marker([49.8951, -97.1384]).addTo(map)
 
 //Add a circle to 
 
@@ -38,4 +77,3 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 
 streets.addTo(map);
-
