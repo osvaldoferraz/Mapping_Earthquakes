75829aa1e32f40aac9b9d753c1458381d280164d
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-27 23:36:16 -0600

    Skill Drill

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,44 +5,39 @@ console.log('working');
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map('mapid', {
     center: [
-        37.6213, -122.3790
+        49, -97
     ],
-    zoom: 5
+    zoom: 4
 }); 
 
 // Coordinates for each point to be used in the line
 let line = [
     [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [30.1975, -97.6664],
+    [49.9098, -97.2365],
+    [43.6777, -79.6248], 
+    [40.6413, -73.7781]
 ]
 
 // Create a polyline using the line coordinates and make the line red
+// L.polyline(line, {
+//     color: "yellow"
+// }).addTo(map)
+
+//Skill Drill
 L.polyline(line, {
-    color: "yellow"
+    color: "blue",
+    dashArray: 10,
+    weight: 4, 
+    opacity: 0.5
 }).addTo(map)
-
-// //Get data from cities.js
-// let cityData = cities
-
-
-// // change the marker for each city to a circle that has the radius equivalent to the city's population
-// cityData.forEach(city => {
-//     console.log(city)
-//     L.circleMarker(city.location, {
-//         radius: city.population/100000
-//     })
-//     .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr><h3>Population ${city.population.toLocaleString()}<h3>`)
-//     .addTo(map);
-// })
 
 
 
 // We create a tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //Sill Drill
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
