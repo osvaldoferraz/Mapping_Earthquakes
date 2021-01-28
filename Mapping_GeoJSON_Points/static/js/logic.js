857e3f0e86aa899e1ee036fe247dae4884756f81
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-28 13:44:33 -0600

    GeoJSON

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,59 +5,6 @@ console.log('working');
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map('mapid').setView([37.5, -122.5],10);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// // Grabbin our GoeJSON Data
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map
-//     pointToLayer: function(feature, latlng) {
-//         console.log(feature);
-//         return L.marker(latlng)
-//         .bindPopup(`<h2>${feature.properties.city}</h2>`)
-//     }
-// }).addTo(map)
-
-// SKILL DRILL
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map
-//     pointToLayer: function(feature, latlng) {
-//         console.log(feature);
-//         return L.marker(latlng)
-//         .bindPopup(`<h3>${feature.properties.name}</h3><hr>
-//         <h4>${feature.properties.city}, ${feature.properties.country}</h4>`)
-//     }
-// }).addTo(map)
-
-// Grabbin our GoeJSON Data
-L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr>
-                        <h4>Airport name: ${feature.properties.name}</h4>`)
-    }
-}).addTo(map)
-
-
-
 // We create a tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //Sill Drill
@@ -68,3 +15,11 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 streets.addTo(map);
+
+// Grabbin our GeoJSON Data
+d3.json(airportData).then(function(data){
+    console.log(data);
+    //Creating a GeoJSON layer with the retrieved data
+    L.geoJason(data).addTo(map);
+})
+
