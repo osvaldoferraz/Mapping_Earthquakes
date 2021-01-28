c719c75397bb9d05e801b418b2c4160c8e1fb96c
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-27 22:55:22 -0600

    Multiple Mapping Cities Completed

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -15,9 +15,33 @@ let map = L.map('mapid', {
 let cityData = cities
 
 // add cities to the map by adding to the marker function
+// cityData.forEach(city => {
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr><h3>Population ${city.population.toLocaleString()}<h3>`)
+//     .addTo(map);
+// })
+
+// change the marker for each city to a circle that has the radius equivalent to the city's population
+// cityData.forEach(city => {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr><h3>Population ${city.population.toLocaleString()}<h3>`)
+//     .addTo(map);
+// })
+
+//Skill Drill
 cityData.forEach(city => {
     console.log(city)
-    L.marker(city.location)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        weight: 4,
+
+    })
+    .bindPopup(`<h2>${city.city}, ${city.state}</h2><hr><h3>Population ${city.population.toLocaleString()}<h3>`)
     .addTo(map);
 })
 
@@ -37,7 +61,7 @@ cityData.forEach(city => {
 // We create a tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //Sill Drill
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
